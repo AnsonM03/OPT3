32ab269dd8e0181666f01189a5879bf986b7ef7f
Anson <23106549@student.hhs.nl>
2025-05-06 12:46:29 +0200

    Beginning

---
+++ .idea/Main/Game.java
@@ -0,0 +1,9 @@
+public class Game {
+    private String naam;
+    private int kamer;
+
+    public Game(String naam, int kamer) {
+        this.naam = naam;
+        this.kamer = kamer;
+    }
+}
---
+++ .idea/Main/Kamer.java
@@ -0,0 +1,9 @@
+public class Kamer {
+    private String naam;
+    private int kamer;
+
+    public Kamer(String naam, int kamer) {
+        this.naam = naam;
+        this.kamer = kamer;
+    }
+}
