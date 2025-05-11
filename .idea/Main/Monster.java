c2d6151b223f85135d2249155c093d3cc2245db7
Nathan R <geraldrasmijn012@gmail.com>
2025-05-11 21:01:06 +0200

    Addded vragen en antwoorden en monster

---
+++ .idea/Main/Monster.java
@@ -0,0 +1,12 @@
+public class Monster {
+    private int schade;
+
+    public Monster(int schade){
+        this.schade = schade;
+    }
+
+    public void valAan(Speler speler){
+        System.out.println("Je heef fout beantwoord, het monster heeft " + schade + "HP van je afgepakt!");
+        speler.neemSchade(schade);
+    }
+}
